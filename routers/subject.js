2390410562a19770c5137ac2ffffb6283071cee2
aditReza <lete.note3@gmail.com>
2017-10-08 16:50:40 +0700

    onprogress enrolledstudents

--- routers/subject.js
+++ routers/subject.js
@@ -24,6 +24,62 @@ router.get('/delete/:id',(req,res)=>{
   })
 })
 
+// add subject get
+router.get('/add', function(req,res){
+  model.Subject.findAll().then(data_Subject => {
+  // projects will be an array of all Project instances
+  // res.send(data_Students)
+  res.render('subject-add', {title:'Subject Add', data_SubjectToEjs:data_Subject})
+  })
+})
+
+// add post subject
+router.post('/add', (req, res) => {
+    //const id         = req.params.id
+    const subject_name = req.body.subject_name
+    model.Subject.create({
+      // id : id,
+      subject_name: subject_name,
+    })
+      .then(function (data_Teacher){
+        res.redirect('../../subject');
+      })
+})
+
+// update get // edit subject
+router.get('/edit/:id', (req,res)=>{
+    model.Subject.findById(req.params.id).then(data_Subjects =>{
+      // res.send(data_Subjects)
+      res.render('subject-edit',{title:'Subject Edit', data_SubjectsToEjs:data_Subjects })
+    })
+})
+
+//update post // post edit
+router.post('/edit/:id',(req,res)=>{
+  const subject_name   = req.body.subject_name
+  model.Subject.update({
+    subject_name:subject_name
+  },{
+    where :{
+      id: req.params.id
+    }
+}).then(function(data_Teacher){
+    res.redirect('/subject')
+  })
+})
+
+// get enrolled
+
+router.get('/:id/enrolledstudents', (req, res) =>{
+  model.StudentSubject.findAll({
+    include : [model.Subject, model.Student]
+  }).then(data_StudentSubject => {
+    // res.send(data_StudentSubject)
+    res.render('subject-enrolledstudents', {title: 'Student Enrolled', data_StudentSubjectjs:data_StudentSubject })
+  })
+})
+
+
 
 
 module.exports = router
